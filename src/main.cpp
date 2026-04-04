0af7ab8af03992ab569b99c4b7f2d8e0df79810a
Kawenka <silvestrekewan@gmail.com>
2026-04-04 20:05:34 +0200

    feat(core): implement core logic in source files

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/VideoMode.hpp>
+
+int main(void) { 
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Skyjo");
+}
