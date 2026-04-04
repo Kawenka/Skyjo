2e2206a85c66a63122b2dd54615c002fe868fb78
Kawenka <silvestrekewan@gmail.com>
2026-04-04 21:11:56 +0200

    feat(card): implement Card attributes and OCC methods

--- inc/Card.hpp
+++ inc/Card.hpp
@@ -2,7 +2,15 @@
 # define CARD_HPP
 
 class Card{
-
+private:
+  int _value;
+  bool _isVisible;
+public:
+  Card();
+  Card( int value );
+  Card( const Card &other );
+  Card &operator=( const Card &other );
+  ~Card();
 };
 
 #endif
---
+++ src/Card.cpp
@@ -0,0 +1,18 @@
+#include "Card.hpp"
+
+Card::Card() : _value(0), _isVisible(false) {}
+
+Card::Card(int value) : _value(value), _isVisible(false) {}
+
+Card::Card(const Card &other) : _value(other._value), _isVisible(other._isVisible) {}
+
+Card &Card::operator=(const Card &other)
+{
+  if (this != &other) {
+    this->_value = other._value;
+    this->_isVisible = other._isVisible;
+  }
+  return *this;
+}
+
+Card::~Card() {}
